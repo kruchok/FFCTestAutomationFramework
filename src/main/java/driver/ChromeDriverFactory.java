db82674a74bdd27069cc3c06b17569e31d8dbd5b
eugeniya.kruchok <eugeniya.kruchok@gmail.com>
2018-06-25 00:28:29 +0300

    Add chrome options

--- src/main/java/driver/ChromeDriverFactory.java
+++ src/main/java/driver/ChromeDriverFactory.java
@@ -3,6 +3,7 @@ package driver;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Created by eugeniya.kruchok on 26.02.2018.
@@ -13,6 +14,11 @@ public class ChromeDriverFactory implements Factory {
     public WebDriver factory() {
 
         ChromeDriverManager.getInstance().setup();
-        return new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        return new ChromeDriver(options);
     }
 }
