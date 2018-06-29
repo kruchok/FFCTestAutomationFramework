d40f0337f1278c7a8c87eeecb430d74886cd781d
eugeniya.kruchok <eugeniya.kruchok@gmail.com>
2018-06-29 09:26:49 +0300

    Add headless mode

--- src/main/java/driver/ChromeDriverFactory.java
+++ src/main/java/driver/ChromeDriverFactory.java
@@ -14,10 +14,15 @@ public class ChromeDriverFactory implements DriverFactory {
     public WebDriver createDriver() {
 
         ChromeDriverManager.getInstance().setup();
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage");
-        return new ChromeDriver();
+        ChromeOptions options;
+        if (System.getProperty("mode", "none").equals("headless")) {
+            options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            return new ChromeDriver(options);
+        } else {
+            return new ChromeDriver();
+        }
     }
 }
